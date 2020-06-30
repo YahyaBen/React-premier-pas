70e6d77a73379fbeedecdc48d1bc8b87e9df8e97
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-06-30 12:08:12 +0100

    React Chappitre 2 - Grafitart

---
+++ app.js
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////Syntaxe classic//////////////////////////////////////////////
+// let n=0
+// function render1() {
+//     const title = React.createElement('h1', {}, 'Bonjour tout le monde', React.createElement('span', {}, n))
+//     ReactDOM.render(title, document.querySelector('#app'))
+//     // console.log(title)
+// }
+// function render(){
+//     document.querySelector("#app").innerHTML = "<h1> Bonjour tout le monde </h1> <span> "+n+"</span>"
+// }
+// render()
+// window.setInterval(() => {
+//     n++
+//     render()
+// }, 1000);
